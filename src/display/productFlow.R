ca978f59735d65bf878c63ada1c6b7170093b161
Jason Brown <jbrown@groupw.com>
2015-05-17 22:00:19 -0400

    Reorganize using src/display subdir for plotting routines

---
+++ src/display/productFlow.R
@@ -0,0 +1,7 @@
+#Plot of overall product flow in the study area 
+
+# map background
+# http://stackoverflow.com/questions/9558040/ggplot-map-with-l
+
+#ribbon plot of journeys; note this one has alpha instead of width for volume.
+# http://spatial.ly/2015/03/mapping-flows/
