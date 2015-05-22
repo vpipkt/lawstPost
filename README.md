cb2190e2a21cfa0d913ca06ae9f6a88f0276500a
Jason Brown <jbrown@groupw.com>
2015-05-22 17:40:52 -0400

    Make `test/testPost.R` a clear intro as well as a basic run to completion check of all vizzes and key data processing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # lawstPost
 Post processing routines for Logistics Analysis and Wargame Support Tool simulation.
+
+## Getting started
+
+Check out `test/testPost.R` for an overview of functionality and how to use the various functions. 
+
+
--- test/testPost.R
+++ test/testPost.R
@@ -1,23 +1,78 @@
-#test post processing functions
+#Test and demonstrate example uses of post processing functions
 
+# Todo: 
+#   * Test/demonstrate absolute path example
+
+#Remove all objects!!
 rm(list=ls())
 
+# Working directory should be lawstPost repo
+getwd()
+
+# Read all source files into the environment.
 source('src/readLawstGame.R')
+source('src/readWriteConsumption.R')
+source('src/combineDeliveries.R')
+source('src/viz/LogNodeStatusHistory.R')
+source('src/viz/LognodeSupply.R')
+source('src/viz/productFlow.R')
 
-#test the config file with absolute paths (need to add an abs path config to example game)
-a <- sapply(readLawstConfig('exampleGames/K/_kdemo.lconfig')$files, file.exists )
-a #should be all true
+# To use any of these functions, you must read in a game configuration file using `readLawstConfig`. If the game is saved using relative paths, set the working directory to the expected LAWST executable directory.
 
-#test a config file with paths relative to LAWST executable.
+# Read unclass Korea demo
 setwd('exampleGames/K/exe')
-rel <- readLawstConfig("../_kdemo.lconfig")
-b <- sapply(rel$files, file.exists )
-b
-mean(b)
+kdemo <- readLawstConfig("../_kdemo.lconfig")
 
-rel.u <- readUnit(rel)
-summary(rel.u)
-head(rel.u)
+# Check the file paths are all good
+mean(sapply(kdemo$files, file.exists)) # should be 1.00
+
+# There are many `read[object]()` functions. They usually return a data frame
+readSupplyTypes(kdemo)
+# Some return a list
+summary(readUnitScript(kdemo))
+summary(readUnitScript(kdemo)$Script)
+
+## Go through the visualizations, some are supported by data extraction functions which will have further utility.
+
+#A basic history of log node's inventory, product flow and transport use
+plotLogNodeHistory(kdemo, 'ANDONG SUPPLY DEPOT')
+
+#What is a log node's supported units on hand inventory?
+plotClientOnHand(kdemo, 'ANDONG SUPPLY DEPOT')
+# Can look at many levels below; use levels = 0 for all
+plotClientOnHand(kdemo, 'ANDONG SUPPLY DEPOT', levels = 0)
+
+# Source data for plotClientOnHand; note it contains demand and consumed amount.
+View(getLognodeClientSupply(kdemo, 'OBJ BLUE SUPPLY POINT'))
+# How does it crawl the log tree?
+View(findDependents(readUnitScript(kdemo)$SupplyScript, 'OBJ BLUE SUPPLY POINT'))
+
+# A more abstract view of downstream 'risk' with all units overplotted
+plotClientRisk(kdemo, 'ANDONG SUPPLY DEPOT')
+
+# A viz of cargo flows by weight
+flowMap(kdemo)
+# Options to limit to a selection of days
+flowMap(kdemo, 8:12)
+# Can limit to an area of interest, 
+flowMap(kdemo, day = 15, xLim = c(128.7, 129.6))
+flowMap(kdemo, 15, xLim = c(128.5, 129.6), yLim = c(34.9, 36.1) )
+# Can also tune the contrast; 0 is transparent and 1 is opaque
+flowMap(kdemo, 15, alphaRange = c(0.01, 0.1))
+flowMap(kdemo, 15, alphaRange = c(0.1, 0.5))
+
+# Underlying data of the flow map
+View(flowData(kdemo))
+# Also useful is a stack of all deliveries 
+summary(readCombinedDeliveries(kdemo)) 
+library(plyr)
+library(reshape2)
+dcast(melt(readCombinedDeliveries(kdemo)), 
+      variable ~ Type + SupplyType, sum, 
+      subset = .(variable %in% c('Volume', 'FuelAmountExpended')))
+
+# Plot of the deliveries
+qplot(x = log(Volume), y = log(FuelAmountExpended), data = readCombinedDeliveries(kdemo), 
+      colour = TransportType)
 
 
-str(readUnitScript(rel))
