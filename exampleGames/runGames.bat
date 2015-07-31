350464659e14704b6f5e0488a6f7c57e33abae02
Jason Brown <jbrown@groupw.com>
2015-07-31 17:53:53 -0400

    Update executable to LawstEmpty repo commit 98ce0c4e; adds batch mode to the application.

---
+++ exampleGames/runGames.bat
@@ -0,0 +1,6 @@
+REM For best results, run with working directory equal to the app directory.
+REM Use -a switch after config file directory to optionally save the files with absolute paths
+REM Otherwise relative path is the default
+
+lawst.exe k\_kdemo.lconfig
+lawst.exe fla\testfla.lconfig
